05ca0826a85ea0c57a054f4de91c24bed6f0a88d
IanLLHonorio <lopeshonorioi@gmail.com>
2023-03-17 07:29:12 -0300

    localhost

--- api_root/settings.py
+++ api_root/settings.py
@@ -141,9 +141,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # Celery
 
-# CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://localhost:6379'
 
-CELERY_BROKER_URL = 'redis://redis:6379'
+# CELERY_BROKER_URL = 'redis://redis:6379'
 
 CELERY_ACCEPT_CONTENT = ['application/json']
 
